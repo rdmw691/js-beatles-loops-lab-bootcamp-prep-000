56946ec3eda50f789e6061ee31b4676cd1d2fdc4
rdmw691@gmail.com <rdmw691@gmail.com>
2018-07-19 20:42:10 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,1 +1,30 @@
 // add solution here
+
+function theBeatlesPlay(musicians, instruments) {
+  var array = []
+  for (var i = 0; i < musicians.length; i++) {
+    array.push(musicians[i] + " plays " + instruments[i])
+  }
+  return array
+}
+
+function johnLennonFacts(facts) {
+  let i = 0
+  var array = []
+  while (i < facts.length) {
+    array.push(facts[i] + "!!!")
+    i++
+  } return array
+}
+
+function iLoveTheBeatles(number) {
+  let i = 0
+  var array = []
+  do {
+    array.push("I love the Beatles!")
+  } while (number[i] < 15 &&maybeTrue())
+  return array
+}
+
+
+function
